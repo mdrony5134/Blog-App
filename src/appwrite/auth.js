af2fc99931cc710c8b5111b4da06b3fd28c639ea
mdrony5134 <rony830388@gamil.com>
2023-11-11 21:13:40 +0600

    auth services done

---
+++ src/appwrite/auth.js
@@ -0,0 +1,62 @@
+/* eslint-disable no-useless-catch */
+import config from "../config/config";
+
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService{
+    client = new Client()
+    account;
+
+    constructor(){
+        this.client
+            .setEndpoint('appWriteUrl')
+            .setProject('appWriteProjectId')
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({email, password, name}){
+        try {
+         const userAccount =  await this.account.create(ID.unique() ,email, password, name)
+            if(userAccount){
+                // call another method
+                return this.login(email, password)
+            }else{
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email, password}){
+        try {
+           return this.account.createEmailSession(email, password)
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async currentUser(){
+        try {
+          return this.account.get()  
+        } catch (error) {
+            console.log('getcurrent user error', error);
+        }
+        return null;
+    }
+
+    async logout(){
+        try {
+           return this.account.deleteSessions() 
+        } catch (error) {
+            console.log('logout error', error);
+        }
+    }
+
+}
+
+
+
+const authService = new AuthService()
+
+export default authService;
